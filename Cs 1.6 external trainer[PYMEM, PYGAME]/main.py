fed66e76b3c9f9411c7282921aeffcb3880e47f2
Cr33pythonny <122759684+Cr33pythonny@users.noreply.github.com>
2023-01-16 19:59:27 +0300

    Update main.py

--- Cs 1.6 external trainer[PYMEM, PYGAME]/main.py
+++ Cs 1.6 external trainer[PYMEM, PYGAME]/main.py
@@ -10,16 +10,16 @@ def main():
 
         multiprocessing.Process(target=updateent).start()
 
-        if get_value_from_cfg('AIMBOT','MISC'):
-            multiprocessing.Process(target=AIMBOT).start()
 
-        if get_value_from_cfg('ESP','MISC'):
+        multiprocessing.Process(target=AIMBOT).start()
+
+        if get_value_from_cfg('ESP','MISC',3):
             multiprocessing.Process(target=ESP).start()
 
-        if get_value_from_cfg('BHOP','MISC'):
+        if get_value_from_cfg('BHOP','MISC',3):
             multiprocessing.Process(target=bhop).start()
             
-        if get_value_from_cfg('DDRUN','MISC'):
+        if get_value_from_cfg('DDRUN','MISC',3):
             multiprocessing.Process(target=ddrun).start()
 
     
